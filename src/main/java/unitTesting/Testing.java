3fed46155cdeaf515bd05440cde04af34435f4cb
jhiguera <j.higuera.f@gmail.com>
2022-09-08 09:18:05 -0300

    cambio display 2

--- src/main/java/unitTesting/Testing.java
+++ src/main/java/unitTesting/Testing.java
@@ -8,7 +8,7 @@ package unitTesting;
 public class Testing {
 
     public static void display(String param){
-        System.out.println("Hello You parameter is " + param);
+        System.out.println("Hello!! You parameter is " + param);
     }
 
     public void someFunc(){
